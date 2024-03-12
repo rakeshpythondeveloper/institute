b34dafbbc402f6b5a9df5ddab2889198776723e2
uday <udayaddepalli261@gmail.com>
2024-03-12 17:17:49 +0530

    add

--- run.py
+++ run.py
@@ -2,7 +2,8 @@
 from Best_It.app import create_app
 
 
-if __name__ == "__main__":      
-    app = create_app()
-    app.run(debug=False) 
+if __name__ == "__main__":    
+      
+    app2 = create_app()
+    app2.run(debug=False) 
      
